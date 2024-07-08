3af70c859ebd58005aa4d1864a43b1a14fd49666
Syed Omar Albeez <syedomar.albeez2022@vitstudent.ac.in>
2024-07-08 13:18:37 +0530

    Updated cors

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = 3001;
 
-app.use(cors({
-  origin: 'https://queryhelpdesk-frontend.vercel.app'
-}));
+const corsOptions = {
+  origin: [
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'https://queryhelpdesk-frontend.vercel.app'
+  ],
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 
 app.get('/hello', (req, res) => {
   res.send('Hello World!');
